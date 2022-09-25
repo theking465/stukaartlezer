d05ca232eecff701bd117164a85c0c22c78405f4
theking465 <vansteenhuyse.daan@gmail.com>
2022-09-25 11:32:39 +0200

    Added scanner program based on pynput lib instead of keyboard lib

---
+++ stukaart-pynput.py
@@ -0,0 +1,87 @@
+from pynput import keyboard as kboard
+import keyboard, requests, json, re
+
+CLIENT_ID='scientica-cursusdienst'
+CLIENT_SECRET='fB9XhdVX7RTiXHklDZ3MAXOcmc6ULf97'
+
+TOKEN_URL="https://idp.kuleuven.be/auth/realms/kuleuven/protocol/openid-connect/token"
+API_ENDPOINT = "https://account.kuleuven.be/api/v1/idverification"
+PATTERN = re.compile(r".*([0-9a-fA-F]{14};[0-9]{10}).*")
+
+scannerInput = ""
+
+def get_access_token(url, client_id, client_secret):
+    response = requests.post(
+        url,
+        data={"grant_type": "client_credentials"},
+        auth=(client_id, client_secret),
+    )
+    return response.json()["access_token"]
+
+def listen_for_scanner():
+    global scannerInput
+    scannerInput = ""
+    while PATTERN.search(scannerInput) is None:
+
+        def on_press(key):
+            try:
+                global scannerInput
+                scannerInput += key.char
+            except AttributeError:
+                pass
+
+        def on_release(key):
+            if key == kboard.Key.enter:
+                # Stop listener
+                return False
+
+        # Collect events until released
+        with kboard.Listener(
+                on_press=on_press,
+                on_release=on_release) as listener:
+            listener.join()
+
+        print(scannerInput)
+        if(scannerInput == ''): keyboard.write('\n')
+        if(scannerInput == 'esc'): exit()
+        if PATTERN.search(scannerInput) is None:
+            print("Invalid input, waiting for retry...")
+    return PATTERN.search(scannerInput).group(1).split(";")
+
+def get_username_from_scanner_details(details):
+    (serial, cardId) = details
+    req = requests.request('POST', API_ENDPOINT, json={
+        "serialNr": serial,
+        "cardAppId": cardId
+    }, headers={
+        "Content-Type": "application/json",
+        "Accept": "*/*",
+        "Authorization" : "Bearer " + AUTH_TOKEN,
+    })
+
+    profile = json.loads(req.text)
+    return profile.get("userName")
+
+def write_username(length, username):
+    for i in range(0,length + 1):
+        keyboard.write("\b")
+    keyboard.write(username)
+    keyboard.write("\n")
+
+def main():
+    print("Press esc+enter twice in order to exit the program")
+    global AUTH_TOKEN
+    print("Getting access token...")
+    AUTH_TOKEN = get_access_token(TOKEN_URL, CLIENT_ID, CLIENT_SECRET)
+    print("Access token received")
+    keyboard.add_abbreviation('enter', '\n')
+    while True:
+        print("Waiting for scanner input...")
+        details = listen_for_scanner()
+        length = len(details[1]) + len(details[0]) + 1
+        print("Correct input, retrieving username...")
+        username = get_username_from_scanner_details(details)
+        write_username(length, username)
+
+if __name__ == "__main__":
+    main()
